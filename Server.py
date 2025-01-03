f651ce6eeab65d87fc36f04df4caa16ea5eb4cec
Joao Sousa <a79300@ualg.pt>
2025-01-03 11:15:36 +0000

    Created validation so the app just detect objects

--- Server.py
+++ Server.py
@@ -1,3 +1,4 @@
+import time
 import socket
 import json
 import threading
@@ -7,11 +8,25 @@ import numpy as np
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
-import time
 
 FILE_PATH = "data.json"
 FRAME_WIDTH, FRAME_HEIGHT = 640, 480
 MODEL_PATH = "assets/efficientdet_lite0.tflite"
+COCO_NAMES_PATH = "assets/coco.names"
+
+
+def load_coco_classes():
+    try:
+        with open(COCO_NAMES_PATH, "r") as f:
+            return [line.strip() for line in f.readlines()]
+    except Exception as e:
+        print(f"Erro ao carregar o arquivo {COCO_NAMES_PATH}: {e}")
+        return []
+
+
+coco_classes = load_coco_classes()
+excluded_classes = ["person"]
+allowed_classes = [cls for cls in coco_classes if cls not in excluded_classes]
 
 
 def load_objects_data():
@@ -45,7 +60,6 @@ def start_server():
     print(f"Servidor ouvindo em {host}:{port}...")
 
     def object_detection_and_hand_detection():
-
         cap = cv2.VideoCapture(0)
         cap.set(cv2.CAP_PROP_FRAME_WIDTH, FRAME_WIDTH)
         cap.set(cv2.CAP_PROP_FRAME_HEIGHT, FRAME_HEIGHT)
@@ -54,25 +68,29 @@ def start_server():
             print("Erro ao acessar a câmera.")
             return
 
-        # Configuração do detector de objetos
         base_options = python.BaseOptions(model_asset_path=MODEL_PATH)
-        options = vision.ObjectDetectorOptions(base_options=base_options, score_threshold=0.5)
+        options = vision.ObjectDetectorOptions(
+            base_options=base_options, score_threshold=0.5
+        )
         detector = vision.ObjectDetector.create_from_options(options)
 
-        # Configuração do Mediapipe para detectar mãos
         mp_hands = mp.solutions.hands
-        hands = mp_hands.Hands(min_detection_confidence=0.7, min_tracking_confidence=0.7)
+        hands = mp_hands.Hands(
+            min_detection_confidence=0.7, min_tracking_confidence=0.7
+        )
 
         try:
             background = cv2.imread("assets/background.png")
             if background is None:
-                background = np.ones((FRAME_HEIGHT, FRAME_WIDTH, 3), dtype=np.uint8) * 255
+                background = (
+                    np.ones((FRAME_HEIGHT, FRAME_WIDTH, 3), dtype=np.uint8) * 255
+                )
             else:
                 background = cv2.resize(background, (FRAME_WIDTH, FRAME_HEIGHT))
         except Exception:
             background = np.ones((FRAME_HEIGHT, FRAME_WIDTH, 3), dtype=np.uint8) * 255
 
-        last_detection_time = time.time()  # Armazenar o tempo da última detecção de objeto
+        last_detection_time = time.time()
 
         while True:
             ret, frame = cap.read()
@@ -82,48 +100,60 @@ def start_server():
             frame = cv2.flip(frame, 1)
             rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-            # Detecção de objetos a cada 3 segundos
             current_time = time.time()
-            if current_time - last_detection_time >= 3:  # A cada 3 segundos
-                # Detecção de objetos
+            if current_time - last_detection_time >= 3:
                 mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
                 detection_result = detector.detect(mp_image)
 
-                # Printar os objetos detectados no console
                 for detection in detection_result.detections:
                     detection_class = detection.categories[0].category_name
                     detection_score = detection.categories[0].score
-                    print(f"Objeto detectado: {detection_class} com confiança de {detection_score:.2f}")
 
-                # Atualizar o tempo da última detecção
+                    if detection_class in allowed_classes:
+                        if detection_class == "cup" and detection_score >= 0.5:
+                            objects_data.append(
+                                {
+                                    "uid": "coffee_table_1",
+                                    "dimensions": [0.002, 0.002, 0.001],
+                                    "location": [0, 0, 0.1],
+                                    "rotation": [90, 0, 0],
+                                    "model": "C:/Users/joaossousa/Desktop/CompVisual/Design3DStudio/Objects/coffee-table.obj",
+                                }
+                            )
+
                 last_detection_time = current_time
 
-            # Copiar o fundo para ser usado como base
             frame_with_background = background.copy()
 
-            # Detecção de mãos
             results = hands.process(rgb_frame)
 
             if results.multi_hand_landmarks:
                 for hand_landmarks in results.multi_hand_landmarks:
-                    # Desenhar as landmarks das mãos
                     for landmark in hand_landmarks.landmark:
                         x = int(landmark.x * FRAME_WIDTH)
                         y = int(landmark.y * FRAME_HEIGHT)
                         cv2.circle(frame_with_background, (x, y), 5, (0, 0, 255), -1)
 
-                    # Desenhar as conexões entre as landmarks das mãos
                     for connection in mp_hands.HAND_CONNECTIONS:
                         start_idx, end_idx = connection
                         start = hand_landmarks.landmark[start_idx]
                         end = hand_landmarks.landmark[end_idx]
 
-                        start_x, start_y = int(start.x * FRAME_WIDTH), int(start.y * FRAME_HEIGHT)
-                        end_x, end_y = int(end.x * FRAME_WIDTH), int(end.y * FRAME_HEIGHT)
+                        start_x, start_y = int(start.x * FRAME_WIDTH), int(
+                            start.y * FRAME_HEIGHT
+                        )
+                        end_x, end_y = int(end.x * FRAME_WIDTH), int(
+                            end.y * FRAME_HEIGHT
+                        )
 
-                        cv2.line(frame_with_background, (start_x, start_y), (end_x, end_y), (0, 255, 0), 2)
+                        cv2.line(
+                            frame_with_background,
+                            (start_x, start_y),
+                            (end_x, end_y),
+                            (0, 255, 0),
+                            2,
+                        )
 
-            # Mostrar a janela com o fundo e as landmarks da mão
             cv2.imshow("Hand Detection", frame_with_background)
 
             if cv2.waitKey(1) & 0xFF == ord("q"):
@@ -163,7 +193,9 @@ def start_server():
         while True:
             try:
                 conn, addr = server_socket.accept()
-                threading.Thread(target=handle_client, args=(conn, addr), daemon=True).start()
+                threading.Thread(
+                    target=handle_client, args=(conn, addr), daemon=True
+                ).start()
             except OSError:
                 print("Erro ao aceitar novas conexões.")
                 break
